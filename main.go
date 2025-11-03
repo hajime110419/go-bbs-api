80e8e7baa5dd437e83695ef59d93a74188919692
Hajime Kugimiya <hajime110419@gmail.com>
2025-11-03 13:25:34 +0900

    feat: Add handler for /posts endpoint

--- main.go
+++ main.go
@@ -10,8 +10,20 @@ func handleRoot(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "掲示板APIへようこそ!")
 }
 
+func handlePosts(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		fmt.Fprint(w, "ここは投稿一覧を返す場所です")
+	case "POST":
+		fmt.Fprint(w, "ここは新しい投稿を作成する場所です")
+	default:
+		http.Error(w, "許可されていないメソッドです", http.StatusMethodNotAllowed)
+	}
+}
+
 func main() {
 	http.HandleFunc("/", handleRoot)
+	http.HandleFunc("/posts", handlePosts)
 
 	port := ":8080"
 	fmt.Printf("サーバーをポート%sで起動します…\n", port)
